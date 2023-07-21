2f326603805dba4a95ac3e924a2c34cd180610f5
saucepoint <saucepoint@protonmail.com>
2023-07-22 00:24:47 +0200

    separate median updates to do running median in the contract

--- src/lib/FrugalMedianLibrary.sol
+++ src/lib/FrugalMedianLibrary.sol
@@ -3,42 +3,50 @@ pragma solidity ^0.8.15;
 
 library FrugalMedianLibrary {
     function frugalMedian(int256[] memory sequence) public pure returns (int256 approxMedian) {
-        int256 num;
         int256 step;
         uint256 i;
         bool positive;
         for (i; i < sequence.length;) {
+            (approxMedian, step, positive) = updateApproxMedian(sequence[i], approxMedian, step, positive);
             unchecked {
-                num = sequence[i];
-                if (num > approxMedian) {
-                    step += positive ? stepIncrement(step) : -stepIncrement(step);
-                    // line 6: we cant do ceiling
-                    approxMedian += (step > 0) ? step : int256(1);
-                    if (approxMedian > num) {
-                        step += num - approxMedian;
-                        approxMedian = num;
-                    }
-                    if (!positive && step > 1) {
-                        step = 1;
-                    }
-                    positive = true;
-                } else if (num < approxMedian) {
-                    step += !positive ? stepIncrement(step) : -stepIncrement(step);
-                    approxMedian -= (step > 0) ? step : int256(1);
-                    // line 18
-                    if (approxMedian < num) {
-                        step += approxMedian - num;
-                        approxMedian = num;
-                    }
-                    if (positive && step > 1) {
-                        step = 1;
-                    }
-                    positive = false;
-                }
-
                 ++i;
             }
         }
+    }
+
+    function updateApproxMedian(int256 newNumber, int256 approxMedian, int256 step, bool positive)
+        public
+        pure
+        returns (int256, int256, bool)
+    {
+        unchecked {
+            if (newNumber > approxMedian) {
+                step += positive ? stepIncrement(step) : -stepIncrement(step);
+                // line 6: we cant do ceiling
+                approxMedian += (step > 0) ? step : int256(1);
+                if (approxMedian > newNumber) {
+                    step += newNumber - approxMedian;
+                    approxMedian = newNumber;
+                }
+                if (!positive && step > 1) {
+                    step = 1;
+                }
+                positive = true;
+            } else if (newNumber < approxMedian) {
+                step += !positive ? stepIncrement(step) : -stepIncrement(step);
+                approxMedian -= (step > 0) ? step : int256(1);
+                // line 18
+                if (approxMedian < newNumber) {
+                    step += approxMedian - newNumber;
+                    approxMedian = newNumber;
+                }
+                if (positive && step > 1) {
+                    step = 1;
+                }
+                positive = false;
+            }
+        }
+        return (approxMedian, step, positive);
     }
 
     function stepIncrement(int256 step) private pure returns (int256) {
